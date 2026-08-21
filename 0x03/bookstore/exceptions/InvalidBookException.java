5f0e6bc8a2b880be5f26f5d6ab6260ab13e2e1cf
ValentinCA28 <12286@holbertonstudents.com>
2026-08-21 13:44:34 +0200

    Revert "0x03: Java Exceptions Part II - salary, bookstore, basic_account, special_account"
    This reverts commit 27df2709e540d5d2340a3b499d77d8e70d4d0c16.

--- 0x03/bookstore/exceptions/InvalidBookException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class InvalidBookException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public InvalidBookException(String message) {
-        super(message);
-    }
-}
