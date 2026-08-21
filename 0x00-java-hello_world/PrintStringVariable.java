4fb4bea6cd71533acc4c8b5c3fb2bbf74291911d
ValentinCA28 <valentin.planchon@yahoo.fr>
2026-08-21 11:23:50 +0200

    0x00 java hello world: chapter 1 solutions

---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,7 @@
+public class PrintStringVariable {
+    public static void main(String[] args) {
+        String text = "Holberton School";
+        System.out.println(text + "\n" + text);
+        System.out.println(text.substring(0, 9));
+    }
+}
